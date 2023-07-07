ab9d4093a5a01f06c2072fd212270966c11881ab
Melo Mateus <100786778+melobrau@users.noreply.github.com>
2023-07-07 14:13:17 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+var msgArray = ["*/ negocio que escreve coisa"];
+var textPosition = 0;
+var speed = 80;
+
+typewriter = () => {
+    document.querySelector("#msg").
+    innerHTML = msgArray[0].substring(0, textPosition) + "<span>\u25ae</span>";
+
+    if (textPosition ++ != msgArray[0].length)
+    setTimeout(typewriter, speed);
+}
+
+window.addEventListener("load", typewriter);
